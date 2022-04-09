c2ba8fa08e25ecf23c8089010a643fb8745af3fa
Lucas dos Santos Amorim Rêgo <lucass235@gmail.com>
2022-04-08 23:37:25 -0300

    final contador

--- script.js
+++ script.js
@@ -1,89 +1,50 @@
-
-let valor = 0
-let valor2 = 4
+let valor = 0;
 let id = 0;
+
 function contador(e) {
-    let elemento = e.target.id;
-    if (elemento == "contador1") {
-        ++valor;
-        var resul = document.getElementById("resultado")
-        resul.textContent = valor;
-    } else if (elemento == "contador2"){
-        ++valor2;
-        var resul = document.getElementById("resultado2")
-        resul.textContent = valor2;
-    }
-    
+  e.innerHTML = ++valor;
 }
 
 function zerar(e) {
-    valor = 0;
-    var resul = document.getElementById("resultado")
-    resul.textContent = valor
+  valor = 0;
+  e.innerHTML = --valor;
 }
 
+//Retorna o próximo ID válido para ao paragrafo que será criada
+function getNextId() {
+  return ++id;
+}
 
+//Sua função, chamada no click do botão
+function criar() {
+  //Pego o ID
+  let id = getNextId();
 
-  //Retorna o próximo ID válido para a DIV que será criada
-  function getNextId() {
-    return ++id;
-  }
+  const title = document.getElementById("titulo").value; //Pego o valor do input do titulo para cada div
+  const div = document.createElement("div"); //Cria uma div
+  const button = document.createElement("button"); //Cria um botão
+  const p = document.createElement("p"); // Cria um parágrafo
+  const p2 = document.createElement("p"); //Cria um parágrafo
 
-  //Sua função, chamada no click do botão
-  function criar(){
-      //Pego o ID
-      let id = getNextId();
-      const body = document.getElementById('body');
-      const text = document.getElementById('titulo').value;
+  p2.id = ++id; //Defino o ID para cada novo parágrafo
 
-      const elementDiv = document.createElement("div");
-      elementDiv.id = id;
-      const elemText = document.createTextNode(text)
-      const elemH1 = document.createElement("h1");
-      const elementButton = document.createElement("button");
-      const labelButton = document.createTextNode("Zerar");
-      const labelContador = document.createElement("label");
-      const textContador = document.createTextNode("Contador: ")
-     labelContador.appendChild(textContador)
-      elementButton.appendChild(labelButton)
-      elemH1.appendChild(elemText)
-      elementDiv.appendChild(elemH1);
-      elementDiv.appendChild(labelContador)
-      elementDiv.appendChild(elementButton)
-      elementDiv.style.backgroundColor = "#f0" + id.toString();
-    //    body.appendChild(elemText);
-       body.appendChild(elementDiv);
-      console.log(elemText);
-      console.log(elementDiv);
-  }
+  p.innerHTML = title; //Defino o texto do parágrafo
+  button.innerHTML = "Zerar"; //Defino o texto do botão
+  p2.innerHTML = valor; //Defino o texto do parágrafo
 
+  div.onclick = function () {
+    contador(p2);
+  }; //Defino o evento de click para cada div
+  button.onclick = function () {
+    zerar(p2);
+  }; //Defino o evento de click para cada botão
 
+  div.appendChild(p); //Adiciona o parágrafo a div
+  div.appendChild(p2); //Adiciona o parágrafo a div
+  div.appendChild(button); //Adiciona o botão a div
 
+  div.style.backgroundColor = "#f0" + id.toString();
 
-
-
-
-
-
-
-
-
-
-//   //Crio a DIV
-//   let divElement = document.createElement("div");
-//   //Pego a DIV onde a nova DIV será criada, sempre na DIV mãe
-//   let divContador = document.getElementById("contador");
-
-//   //A ideia do ID é que ele seja um elemento único, ou seja, não se repita
-//   divElement.setAttribute('id', 'box' + id.toString());
-
-//   //CSS
-//   divElement.style.width = "100%";
-//   divElement.style.height = "50px";
-
-//   //Essa parte é mais para deixar claro que outras divs estão sendo criadas, criando um degrade
-//   divElement.style.backgroundColor = "#f0" + id.toString();
-
-//   //Adiciono a nova DIV na DIV mãe
-//   //Aqui poderia ser por exemplo document.body.appendChild, adicionando assim o elemento criado diretamente no body
-//   divContador.appendChild(divElement);
+  document.querySelector(".space").appendChild(div); //Adiciona a div criada ao documento
+  console.log(div);
+}
